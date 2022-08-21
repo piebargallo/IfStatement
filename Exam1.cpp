0c9f02f0d3e3e33644a563fec01ce8a843395caa
piebargallo <90567646+piebargallo@users.noreply.github.com>
2022-08-21 10:49:13 +0200

    Add files via upload

---
+++ Exam1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Driver code 
+int main () {
+
+   int a = 10;
+ 
+ 	// If condition is true then print the following
+   if( a < 20 ) 
+      
+	  printf("a is less than 20\n" );
+   
+   printf("value of a is : %d", a);
+ 
+   return 0;
+
+} // End driver
